925ac7ad4634e51521d785d83811df1d53b8b761
DucHai <toilahai05@gmail.com>
2026-07-03 00:29:15 +0700

    api ip #4

--- Frontend/src/api/httpClient.js
+++ Frontend/src/api/httpClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const GATEWAY_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+export const GATEWAY_URL = import.meta.env.VITE_API_URL || 'http://192.168.1.246:5173';
 
 export const apiClient = axios.create({
     baseURL: GATEWAY_URL,
